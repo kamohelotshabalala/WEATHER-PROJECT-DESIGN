33f8c0380898a58858518e261f4907a366f38507
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-07 20:44:18 +0200

    removed written text to replace it with integrated API

--- index.js
+++ index.js
@@ -11,10 +11,35 @@ searchFormElement.addEventListener("submit", searchSubmit);
 
 function displayWeather(response){
 let temperatureElement = document.querySelector("#weather-temperature");
+let emojiElement = document.querySelector("#emoji");
+let humidityElement = document.querySelector("#humidity");
+let speedElement = document.querySelector("#speed");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+let iconElement = document.querySelector("#icon");
+iconElement.innerHTML = `<imgsrc = "${response.data.condition.icon_url}" class "weather-icon"`;
+
+
 let temperature = response.data.temperature.current;
 temperatureElement.innerHTML = Math.round(temperature);
+emojiElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML =` ${ response.data.temperature.humidity }%`;
+speedElement.innerHTML = ` ${ response.data.wind.speed}km/h`;
+timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date){
+    let day = days[date.getDay()];
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    if (minutes < 10){
+        minutes = `0 ${minutes}`;
+    }
+
+return `${day} ${hours} ${minutes}`;
+}
 
 function searchCity(city) {
     let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f ";
