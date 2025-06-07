0c663590ee01b01f3970d36cfd16f8e58581fa3d
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-07 19:06:09 +0200

    Updated search functionality using Api

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
+
+function displayWeather(response){
+let temperatureElement = document.querySelector("#weather-temperature");
+let temperature = response.data.temperature.current;
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchSubmit);
+function searchCity(city) {
+    let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f ";
+    let apiUrl =
+    'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric' ;
+    axios.get(apiUrl).then(displayWeather);
+}
+searchCity("Paris");
