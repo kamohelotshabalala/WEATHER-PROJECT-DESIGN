9fa1e80d289ae0c11955a0aff862278340fd168f
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-08 19:58:00 +0200

    Changed (') symbol to (`) in my ApiUrl to make it work then added icon and time to my index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ let emojiElement = document.querySelector("#emoji");
 let humidityElement = document.querySelector("#humidity");
 let speedElement = document.querySelector("#speed");
 let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
 let iconElement = document.querySelector("#icon");
-iconElement.innerHTML = `<imgsrc = "${response.data.condition.icon_url}" class "weather-icon"`;
+iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class "weather-icon"/>`;
+let date = new Date(response.data.time * 1000);
 
 
 let temperature = response.data.temperature.current;
@@ -29,22 +29,19 @@ timeElement.innerHTML = formatDate(date);
 }
 
 function formatDate(date){
-    let day = days[date.getDay()];
     let minutes = date.getMinutes();
     let hours = date.getHours();
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+    let day = days[date.getDay()];
     if (minutes < 10){
-        minutes = `0 ${minutes}`;
+        minutes = `0${minutes}`;
     }
-
-return `${day} ${hours} ${minutes}`;
+return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-    let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f ";
+    let apiKey = "46o4ft0aa94fd28cbcf4b985bc283d4f" ;
     let apiUrl =
-    'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric' ;
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric` ;
     axios.get(apiUrl).then(displayWeather);
 }
-searchCity("Paris");
